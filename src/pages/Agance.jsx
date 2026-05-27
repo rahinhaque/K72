af8e0bd1daf4c5e97366409c686f5e069a6fc91a
Rahin Haque <haquerahin743@gmail.com>
2026-05-27 14:30:58 +0600

    Installed GSAP, Designed Agence page using GSAP and added Image animation

--- src/pages/Agance.jsx
+++ src/pages/Agance.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
 
 const Agance = () => {
- return (
-  <div>
-   sav
-  </div>
- );
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageArray = [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+
+  ];
+  // console.log(imageArray);
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 33.6%",
+        end: "top -70%",
+        pin: true,
+        onUpdate: (element) => {
+         if(element.progress > 0 && element.progress < 1){
+           const imageIndex = Math.round(
+             element.progress * (imageArray.length - 1),
+           );
+           imageRef.current.src = imageArray[imageIndex];
+         }
+        }
+      },
+    });
+  });
+
+  return (
+    <div>
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="h-[20vw] w-[15vw] absolute top-80 left-[30vw] overflow-hidden"
+        >
+          <img ref={imageRef}
+            className="rounded-4xl object-cover"
+            src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
+            alt=""
+          />
+        </div>
+
+        <div className="font-[font2] relative">
+          <div className="mt-[55vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              Soixan7e
+              <br />
+              Douze
+            </h1>
+          </div>
+
+          <div className="pl-[40%] mt-20">
+            <p className="text-6xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="section2"></div>
+    </div>
+  );
 };
 
 export default Agance;
